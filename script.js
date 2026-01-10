8ac8bb51c3cd5c1bfba556d97c97cdf745034c46
Alex <agscheaua@gmail.com>
2026-01-10 23:04:03 +0200

    creen logic, text disapears when input something w ith textContent.

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function addNum (para1, para2) {
         return para1 + para2;
     };
 };
-
 function substractNum (para1, para2) {
     if ( typeof para1 !== 'number' || typeof para2 !== 'number' ) {
         return invalidInput;
@@ -18,7 +17,6 @@ function substractNum (para1, para2) {
         return para1 - para2;
     };
 }
-
 function multiplyNum (para1, para2) {
     if ( typeof para1 !== 'number' || typeof para2 !== 'number' ) {
         return invalidInput;
@@ -27,7 +25,6 @@ function multiplyNum (para1, para2) {
         return para1 * para2;
     };
 }
-
 function divideNum (para1, para2) {
     if ( typeof para1 !== 'number' || typeof para2 !== 'number' ) {
         return invalidInput;
@@ -36,3 +33,39 @@ function divideNum (para1, para2) {
         return para1 / para2;
     }
 }
+
+function operate (operator, firstOperand, secondOperand) {
+    if (operator === '+') {
+        return addNum(firstOperand, secondOperand);
+    }
+    else if (operator === '-') {
+        return substractNum(firstOperand, secondOperand);
+    }
+    else if (operator === '*') {
+        return multiplyNum(firstOperand, secondOperand);
+    }
+    else if (operator === '/') {
+      return divideNum(firstOperand, secondOperand);
+    }
+    else {
+        return invalidInput;
+    };
+};
+
+function inputCalculation () {
+    let firstNumber;
+    let operatorSign;
+    let secondNumber;
+
+    const containerCalculator = document.querySelector('.containerCalculator');
+    containerCalculator.addEventListener('click', (item) => {
+        firstNumber = item.target.textContent;
+        containerScreen.textContent = firstNumber; 
+        console.log(firstNumber);
+    } ); 
+    
+    const containerScreen = document.querySelector('.containerScreen');
+
+
+};
+console.log( inputCalculation() ); 
